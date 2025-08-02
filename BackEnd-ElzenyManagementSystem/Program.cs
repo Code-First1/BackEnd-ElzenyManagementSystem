cc87829f6a9ef8d8539587c4e82a5012230c2376
abdallahyoussriU3 <abdullahyoussri3435@gmail.com>
2025-08-02 23:28:15 +0300

    Product Module - DbContext, Configurations,  Migrations

--- BackEnd-ElzenyManagementSystem/Program.cs
+++ BackEnd-ElzenyManagementSystem/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+
 namespace BackEnd_ElzenyManagementSystem
 {
     public class Program
@@ -13,6 +16,12 @@ namespace BackEnd_ElzenyManagementSystem
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<ElzenyDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            }
+            );
 
             var app = builder.Build();
 
---
+++ Infrastructure/Persistence/Data/ElzenyDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Data
+{
+    public class ElzenyDbContext : DbContext
+    {
+        public ElzenyDbContext(DbContextOptions<ElzenyDbContext> options) : base(options) 
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyRefrence).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
