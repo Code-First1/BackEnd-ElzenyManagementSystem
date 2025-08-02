9b18ba8d683dce4f56923950e4b19f88dfa36869
abdallahyoussriU3 <abdullahyoussri3435@gmail.com>
2025-08-02 23:02:28 +0300

    Product Module - Entities

---
+++ Core/Domain/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Product : BaseEntity<int>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+
+        //Forign Keys
+        public int CategoryId { get; set; }
+
+
+        //Navigation Properties
+        public Category Category { get; set; } 
+    }
+}
