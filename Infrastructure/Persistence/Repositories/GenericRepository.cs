888d138f6bff68f20d120b4c1b3d51f7fc034f46
abdallahyoussriU3 <abdullahyoussri3435@gmail.com>
2025-08-04 12:10:27 +0300

    Implement Generic Repository

---
+++ Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using Domain.Contracts;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Formats.Tar;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private ElzenyDbContext _context;
+        public GenericRepository(ElzenyDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<TEntity>> GetAllAsync(bool trackChages = false)
+        {
+            if(trackChages)
+                return await _context.Set<TEntity>().ToListAsync();
+            return await _context.Set<TEntity>().AsNoTracking().ToListAsync();
+
+        }
+
+        public async Task<TEntity?> GetAsync(TKey id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);    
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _context.AddAsync(entity);
+        }
+
+        public async Task Update(TEntity entity)
+        {
+             _context.Update(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _context.Remove(entity);
+            
+        }
+
+        
+    }
+}
