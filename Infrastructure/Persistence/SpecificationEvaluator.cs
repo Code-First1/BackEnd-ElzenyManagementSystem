fc1452d4acb1b2998fc2f4fe1e9f971a3e4687c7
abdallahyoussriU3 <abdullahyoussri3435@gmail.com>
2025-08-04 23:02:06 +0300

    Build SpecificationEvaluator

---
+++ Infrastructure/Persistence/SpecificationEvaluator.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Contracts;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class SpecificationEvaluator
+    {
+        //Generate Query With any Specifications
+        public static IQueryable<TEntity> GetQuery<TEntity,TKey>
+            (
+            IQueryable<TEntity> inputQuery,
+            ISpecifications<TEntity,TKey> spec
+            ) where TEntity : BaseEntity<TKey>
+        {
+            var query = inputQuery;
+            if(spec.Criteria is not null)
+            {
+                query =  query.Where(spec.Criteria);
+            }
+
+            query =  spec.IncludeExpressions.Aggregate(query, (currentQuery, IncludeExpression) => currentQuery.Include(IncludeExpression));
+
+            return query;
+        }
+    }
+}
