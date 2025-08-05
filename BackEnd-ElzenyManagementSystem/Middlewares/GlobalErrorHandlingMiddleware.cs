eed2fbf76dd4cb7ae5d42d4ff73368c04b78f9a6
abdallahyoussriU3 <abdullahyoussri3435@gmail.com>
2025-08-05 22:48:11 +0300

    Handle Not Found End Point Case

--- BackEnd-ElzenyManagementSystem/Middlewares/GlobalErrorHandlingMiddleware.cs
+++ BackEnd-ElzenyManagementSystem/Middlewares/GlobalErrorHandlingMiddleware.cs
@@ -18,6 +18,16 @@ namespace BackEnd_ElzenyManagementSystem.Middlewares
             try
             {
                 await _next.Invoke(context);
+                if(context.Response.StatusCode == StatusCodes.Status404NotFound)
+                {
+                    context.Response.ContentType = "application/json";
+                    var response = new ErrorDetails()
+                    {
+                        StatusCode = StatusCodes.Status404NotFound,
+                        ErrorMessage = $"End Point {context.Request.Path} is Not Found"
+                    };
+                    await context.Response.WriteAsJsonAsync(response);
+                }
             }
             catch (Exception ex)
             {
