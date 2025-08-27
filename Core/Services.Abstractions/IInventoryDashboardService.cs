a3f6634e33171defa1f6c024702d2b2715d9e051
abdallahyoussriU3 <abdullahyoussri3435@gmail.com>
2025-08-27 16:24:01 +0300

    Handle InventoryDashboard

---
+++ Core/Services.Abstractions/IInventoryDashboardService.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+using Shared.DTOs.InventoryDashboard;
+using Shared.DTOs.InventoryProduct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Abstractions
+{
+    public interface IInventoryDashboardService
+    {
+        Task<InventoryDashboardCountDto> GetProductsCountAsync();
+
+        Task<IEnumerable<InventoryProductResultDto>> GetGoodProductsAsync();
+        Task<IEnumerable<InventoryProductResultDto>> GetCriticalProductsAsync();
+        Task<IEnumerable<InventoryProductResultDto>> GetEmptyProductsAsync();
+    }
+}
---
+++ Shared/DTOs/InventoryDashboard/InventoryDashboardCountDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs.InventoryDashboard
+{
+    public class InventoryDashboardCountDto
+    {
+        public int GoodProductsCount { get; set; }
+        public int CriticalProductsCount { get; set; }
+        public int EmptyProductsCount { get; set; }
+    }
+}
